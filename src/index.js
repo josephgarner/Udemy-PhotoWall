044f9b00beb138431e9c88fbc72f1a5e4464fa65
Joseph Garner <joseph.peter.garner@gmail.com>
2020-01-05 21:27:41 +1100

    Section 5 Complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Main from './Components/Main'
-
+import './styles/main.css'
 
 ReactDOM.render(<Main/>, document.getElementById('root'))
