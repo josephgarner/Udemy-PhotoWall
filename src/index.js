f9114b94164ad1b167109ad202e775abdda903f8
Joseph Garner <joseph.peter.garner@gmail.com>
2020-01-07 20:10:19 +1100

    lecture 4 complete

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom'
 import Main from './Components/Main'
 import './styles/main.css'
 import {BrowserRouter} from 'react-router-dom'
+import {createStore} from 'redux'
+import rootReducer from './redux/reducer'
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(<BrowserRouter><Main/></BrowserRouter>, document.getElementById('root'))
